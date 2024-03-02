4af5c0c82eb7e9636127a7fe507824609255de30
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-03-02 08:33:18 +0100

    add List of Quotes (to use in users inventory)

--- Inventory.cs
+++ Inventory.cs
@@ -4,8 +4,15 @@ public class Inventory
 {
     public List<Product> Products { get; } = new List<Product>();
     
-    public void AddItem(Product product)
+    public void AddProduct(Product product)
     {
         Products.Add(product);
+    }
+    
+    public List<Quote> Quotes { get; } = new List<Quote>();
+    
+    public void AddQuote(Quote quote)
+    {
+        Quotes.Add(quote);
     }
 }
